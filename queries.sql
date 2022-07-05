230ce25ead4d1988e569fc8767bbbfc21d64f454
Jake Felice <jasonfelice0@gmail.com>
2022-07-05 04:03:00 -0700

    Craete queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name LIKE '%mon';
+SELECT * from animals WHERE date_of_birth > '2016-1-1' and date_of_birth < '2019-12-21';
+SELECT * from animals WHERE neutered = true and escape_attempts < 3;
+SELECT date_of_birth from animals WHERE name = 'Agumon' or name ='Pikachu';
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+SELECT * from animals WHERE neutered = true;
+SELECT * from animals WHERE name NOT LIKE 'Gabumon';
+SELECT * from animals WHERE weight_kg;
+SELECT * from animals WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
