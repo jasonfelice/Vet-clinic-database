08dc4c901dad29b82af5ebc85c9a029b0e6e230c
Jake Felice <jasonfelice0@gmail.com>
2022-07-05 03:39:29 -0700

    Create schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
+create database vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts INT,
+    neutered boolean,
+    weight_kg decimal
 );
